d6480cffdb9e7dfc897f82fb8c20cafcd211da31
Niz712 <st1remain@gmail.com>
2018-10-12 15:27:09 +0800

    add README

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Go-selpg
 CLI 命令行实用程序开发基础-selpg
+
+
+
+- Install
+
+  ```
+  go get github.com/Niz712/Go-selpg
+  ```
+
+
+
+- Usage
+
+![usage](/images/usage.png)
+
+
+
+## 测试
+
+1. `$ selpg -s1 -e1 input_file`
+
+   该命令将把“input_file”的第 1 页写至标准输出（也就是屏幕），因为这里没有重定向或管道。
+
+   ![1](/images/1.png)
+
+2. `$ selpg -s1 -e1 < input_file`
+
+   该命令与示例 1 所做的工作相同，但在本例中，selpg 读取标准输入，而标准输入已被 shell／内核重定向为来自“input_file”而不是显式命名的文件名参数。输入的第 1 页被写至屏幕。
+
+   ![2](/images/2.png)
+
+3. `other_command | selpg -s10 -e20`
+
+   “other_command”的标准输出被 shell／内核重定向至 selpg 的标准输入。将第 10 页到第 20 页写至 selpg 的标准输出（屏幕）。
+
+   ![3](/images/3.png)
+
+4. `$ selpg -s10 -e20 input_file >output_file`
+
+   selpg 将第 10 页到第 20 页写至标准输出；标准输出被 shell／内核重定向至“output_file”。
+
+   ![4](/images/4.png)
--- selpg.go
+++ selpg.go
@@ -26,8 +26,8 @@ func main() {
 	progname = os.Args[0]
 
 	var sa selpg_args
-	processArgs(&sa)
-	processInput(&sa)
+	processArgs( & sa)
+	processInput( & sa)
 }
 
 func errExit() {
@@ -37,12 +37,12 @@ func errExit() {
 	}
 }
 
-func processArgs(sa *selpg_args) {
-	flag.IntVarP(&sa.start_page, "start-page", "s", 1, "start page number")
-	flag.IntVarP(&sa.end_page, "end-page", "e", 1, "end page number")
-	flag.IntVarP(&sa.page_len, "page-length", "l", 72, "lines per page")
-	flag.BoolVarP(&sa.form_feed_delimited, "form-feed-delimited", "f", false, "form feed delimited")
-	flag.StringVarP(&sa.print_dest, "print-dest", "d", "", "print dest")
+func processArgs(sa * selpg_args) {
+	flag.IntVarP( & sa.start_page, "start-page", "s", 1, "start page number")
+	flag.IntVarP( & sa.end_page, "end-page", "e", 1, "end page number")
+	flag.IntVarP( & sa.page_len, "page-length", "l", 72, "lines per page")
+	flag.BoolVarP( & sa.form_feed_delimited, "form-feed-delimited", "f", false, "form feed delimited")
+	flag.StringVarP( & sa.print_dest, "print-dest", "d", "", "print dest")
 	flag.Parse()
 	sa.in_filename = ""
 	if len(flag.Args()) > 0 {
@@ -50,16 +50,16 @@ func processArgs(sa *selpg_args) {
 	}
 }
 
-func processInput(sa *selpg_args) {
+func processInput(sa * selpg_args) {
 	var page_count int
 
-	reader := makeReader(sa)
-	writer, sub_proc := makeWriter(sa)
+	reader: = makeReader(sa)
+	writer, sub_proc: = makeWriter(sa)
 
-	if !sa.form_feed_delimited {
-		page_count = pagingL(reader, &writer, sa)
-	} else {
-		page_count = pagingF(reader, &writer, sa)
+	if ! sa.form_feed_delimited {
+		page_count = pagingL(reader,  & writer, sa)
+	}else {
+		page_count = pagingF(reader,  & writer, sa)
 	}
 
 	warnAtPageCount(page_count, sa)
@@ -71,18 +71,18 @@ func processInput(sa *selpg_args) {
 	fmt.Fprintf(os.Stderr, "%s: done\n", progname)
 }
 
-func makeReader(sa *selpg_args) *bufio.Reader {
-	in_fd := os.Stdin
+func makeReader(sa * selpg_args) * bufio.Reader {
+	in_fd: = os.Stdin
 	if len(sa.in_filename) > 0 {
 		in_fd, err = os.Open(sa.in_filename)
 		errExit()
 	}
-	return bufio.NewReaderSize(in_fd, 16*1024)
+	return bufio.NewReaderSize(in_fd, 16 * 1024)
 }
 
-func makeWriter(sa *selpg_args) (io.Writer, *exec.Cmd) {
+func makeWriter(sa * selpg_args)(io.Writer,  * exec.Cmd) {
 	var writer io.Writer
-	var sub_proc *exec.Cmd
+	var sub_proc * exec.Cmd
 
 	writer = os.Stdout
 	if len(sa.print_dest) > 0 {
@@ -96,10 +96,10 @@ func makeWriter(sa *selpg_args) (io.Writer, *exec.Cmd) {
 	return writer, sub_proc
 }
 
-func pagingL(reader *bufio.Reader, writer *io.Writer, sa *selpg_args) int {
+func pagingL(reader * bufio.Reader, writer * io.Writer, sa * selpg_args)int {
 	var line string
-	line_ctr := 0
-	page_ctr := 1
+	line_ctr: = 0
+	page_ctr: = 1
 
 	for {
 		line, err = reader.ReadString('\n')
@@ -114,15 +114,15 @@ func pagingL(reader *bufio.Reader, writer *io.Writer, sa *selpg_args) int {
 			line_ctr = 1
 		}
 		if (page_ctr >= sa.start_page) && (page_ctr <= sa.end_page) {
-			fmt.Fprintf(*writer, line)
+			fmt.Fprintf( * writer, line)
 		}
 	}
 	return page_ctr
 }
 
-func pagingF(reader *bufio.Reader, writer *io.Writer, sa *selpg_args) int {
+func pagingF(reader * bufio.Reader, writer * io.Writer, sa * selpg_args)int {
 	var page string
-	page_ctr := 1
+	page_ctr: = 1
 
 	for {
 		page, err = reader.ReadString('\f')
@@ -133,20 +133,20 @@ func pagingF(reader *bufio.Reader, writer *io.Writer, sa *selpg_args) int {
 
 		page_ctr++
 		if (page_ctr >= sa.start_page) && (page_ctr <= sa.end_page) {
-			fmt.Fprintf(*writer, page)
+			fmt.Fprintf( * writer, page)
 		}
 	}
 	return page_ctr
 }
 
-func warnAtPageCount(page_count int, sa *selpg_args) {
+func warnAtPageCount(page_count int, sa * selpg_args) {
 	if page_count < sa.start_page {
-		fmt.Fprintf(os.Stderr, "%s: start_page (%d) greater than total pages (%d),"+
-			" no output written\n",
+		fmt.Fprintf(os.Stderr, "%s: start_page (%d) greater than total pages (%d)," + 
+			" no output written\n", 
 			progname, sa.start_page, page_count)
-	} else if page_count < sa.end_page {
-		fmt.Fprintf(os.Stderr, "%s: end_page (%d) greater than total pages (%d),"+
-			" less output than expected\n",
+	}else if page_count < sa.end_page {
+		fmt.Fprintf(os.Stderr, "%s: end_page (%d) greater than total pages (%d)," + 
+			" less output than expected\n", 
 			progname, sa.end_page, page_count)
 	}
 }
